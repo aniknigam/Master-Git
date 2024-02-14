d2bf507c77674fbd344745efdf8298e66f0d00d2
Aniket Nigam <108569163+aniknigam@users.noreply.github.com>
2024-02-14 22:33:21 +0530

    update for learning pull

--- Git.java
+++ Git.java
@@ -1,5 +1,5 @@
 public class Git {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World" + "I am writing this text on github for pull");
       }
 }
