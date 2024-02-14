21eb2bd79aabae7b879174b856c9a5a1f501700f
Aniket Nigam <aniknigam2@gmail.com>
2024-02-14 23:30:39 +0530

    successfuly learn how to revert

--- Git.java
+++ Git.java
@@ -1,5 +1,8 @@
 public class Git {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-      }
+        System.out.println("Hello World" + "I am writing this text on github for pull");
+        System.out.println("using stash command");
+        System.out.println("hello");
+        System.out.println("world");
+    }
 }
