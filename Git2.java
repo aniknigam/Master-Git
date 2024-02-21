d14ebce4464b0d1b4a31c8ee7377bb65ded8e8e4
Aniket Nigam <108569163+aniknigam@users.noreply.github.com>
2024-02-21 21:47:04 +0530

    Update Git2.java

--- Git2.java
+++ Git2.java
@@ -1,7 +1,7 @@
 public class Git2 {
     public static void main(String[] args) {
         System.out.println("second file");
-           System.out.println("second line added from github");
+           System.out.println("second line added from github and making more changes");
         
     }
 }
