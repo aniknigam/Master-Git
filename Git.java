15bcd03686fd748e4251f3c0f64c074053fd8aa1
Aniket Nigam <aniknigam2@gmail.com>
2024-02-21 21:28:54 +0530

    eng class added

--- Git.java
+++ Git.java
@@ -1,15 +1,21 @@
 public class Git {
-   public static void main(String[] args) {
-    
-   }
+    public static void main(String[] args) {
+
+    }
 }
 
 class Mathmatics {
     public int add(int a, int b) {
         return a + b;
     }
-    
+
     public int subtract(int a, int b) {
         return a - b;
+    }
+}
+
+class English {
+    public String getMessage() {
+        return "hello my name is aniket nigam and i am learning git";
     }
 }
