7f8861c4e0ef56ed0b395309d078ba3facd6ef73
Aniket Nigam <aniknigam2@gmail.com>
2024-02-21 21:14:56 +0530

    calculation code

--- Git.java
+++ Git.java
@@ -1,10 +1,15 @@
 public class Git {
-    public static void main(String[] args) {
-        System.out.println("Hello World" + "I am writing this text on github for pull");
-        System.out.println("using stash command");
-        System.out.println("hello");
-        System.out.println("world");
-        System.out.println("java");
-        System.out.println("for merge");
+   public static void main(String[] args) {
+    
+   }
+}
+
+class Mathmatics {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+    public int subtract(int a, int b) {
+        return a - b;
     }
 }
